e9081af012f7b0ef7c57fae5e0332edd780cdf75
Jean-Michel De Bosschere <jmdb.idb@icloud.com>
2020-03-14 16:51:45 +0100

    Initial Commit

---
+++ Les Calanques/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Les Calanques
+//
+//  Created by jean-Michel De bosschere on 14/03/2020.
+//  Copyright © 2020 IDB. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
