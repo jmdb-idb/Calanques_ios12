0d067856b7465e1b9be2aa04ce83c34b74fbbf65
Jean-Michel De Bosschere <jmdb.idb@icloud.com>
2020-03-15 11:38:41 +0100

    Ajout des nouveaux Controllers

---
+++ Les Calanques/Controller/ControllerAvecTableView.swift
@@ -0,0 +1,30 @@
+//
+//  ControllerAvecTableView.swift
+//  Les Calanques
+//
+//  Created by jean-Michel De bosschere on 15/03/2020.
+//  Copyright © 2020 IDB. All rights reserved.
+//
+
+import UIKit
+
+class ControllerAvecTableView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
